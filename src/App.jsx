8c2bd558b19d471f2516b218993208520d24f013
wonbindasilva <bloomhw4@gmail.com>
2024-08-19 23:12:58 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import {useState} from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem';
+
+const App = () => {
+
+const [color] = useState("#0000FF")
+document.body.style.backgroundColor = color
+}
+
+return (
+<Routes>
+<Route path="/" element={<Listagem />}/> 
+
+</Routes>
+)
+export default App;
